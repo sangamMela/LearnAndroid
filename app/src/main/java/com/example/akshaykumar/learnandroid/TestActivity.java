deb1aad6022b78043f0acb6fcdfeeb22f9ecea0f
akshaykumar <kumarakshay26@gmail.com>
2016-09-24 21:25:49 +0530

    BroadCast Receiver Implemented

---
+++ app/src/main/java/com/example/akshaykumar/learnandroid/TestActivity.java
@@ -0,0 +1,351 @@
+package com.example.akshaykumar.learnandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.akshaykumar.learnandroid.adapter.DrawerItemCustomAdapter;
+import com.example.akshaykumar.learnandroid.data.DataModel;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class TestActivity extends AppCompatActivity {
+
+    private String[] mNavigationDrawerItemTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    Toolbar toolbar;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    ActionBarDrawerToggle mDrawerToggle;
+
+    static final int NUM_ITEMS = 6;
+    int currentPage = 0;
+    Handler handler;
+    Runnable update;
+    ImageFragmentPagerAdapter imageFragmentPagerAdapter;
+    ViewPager viewPager;
+    public static final String[] IMAGE_NAME = {"banner1", "banner2", "banner3", "banner4", "banner5", "banner6",};
+
+
+    LinearLayout mapsMenu;
+    LinearLayout videosMenu;
+    LinearLayout detailsMenu;
+    LinearLayout eventsMenu;
+    EditText mLocationAddress;
+    TextView mLocationText;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+        imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(imageFragmentPagerAdapter);
+
+//        LinePageIndicator linePageIndicator = (LinePageIndicator)findViewById(R.id.titles);
+//        linePageIndicator.setViewPager(viewPager);
+
+        CircleIndicator indicator = (CircleIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
+
+
+        mapsMenu = (LinearLayout) findViewById(R.id.mapsMenu);
+        videosMenu = (LinearLayout) findViewById(R.id.videosMenu);
+        detailsMenu = (LinearLayout) findViewById(R.id.detailsMenu);
+        eventsMenu = (LinearLayout) findViewById(R.id.eventsMenu);
+
+
+        mTitle = mDrawerTitle = getTitle();
+        mNavigationDrawerItemTitles = getResources().getStringArray(R.array.navigation_drawer_items_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        setupToolbar();
+
+
+        handler = new Handler();
+        update = new Runnable() {
+            public void run() {
+                if (currentPage == NUM_ITEMS) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, 0, 2000);
+
+
+        DataModel[] drawerItem = new DataModel[10];
+
+        drawerItem[0] = new DataModel(R.drawable.house, "Home");
+        drawerItem[1] = new DataModel(R.drawable.placeholder, "Mela Map");
+        drawerItem[2] = new DataModel(R.drawable.chat, "Guru Details");
+        drawerItem[3] = new DataModel(R.drawable.glasses, "Mela Videos");
+        drawerItem[4] = new DataModel(R.drawable.magnifying, "Mela Events");
+        drawerItem[5] = new DataModel(R.drawable.piggy, "Food");
+        drawerItem[6] = new DataModel(R.drawable.heart, "Health");
+        drawerItem[7] = new DataModel(R.drawable.phone, "Emergency");
+        drawerItem[8] = new DataModel(R.drawable.settings, "Settings");
+        drawerItem[9] = new DataModel(R.drawable.paperplane, "About Us");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.list_view_item_row, drawerItem);
+        mDrawerList.setAdapter(adapter);
+
+
+        LayoutInflater layoutinflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup) layoutinflater.inflate(R.layout.nav_header_image, mDrawerList, false);
+        mDrawerList.addHeaderView(header);
+
+        mDrawerList.setItemChecked(0, true);
+        mDrawerList.setSelection(0);
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        setupDrawerToggle();
+
+
+        mapsMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TestActivity.this, MapsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        videosMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TestActivity.this, VideosActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        detailsMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TestActivity.this, InformationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        eventsMenu.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TestActivity.this, EventsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.akshaykumar.learnandroid/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.akshaykumar.learnandroid/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    public static class ImageFragmentPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ImageFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            SwipeFragment fragment = new SwipeFragment();
+            return SwipeFragment.newInstance(position);
+        }
+    }
+
+    public static class SwipeFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View swipeView = inflater.inflate(R.layout.swipe_fragment, container, false);
+            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView);
+            Bundle bundle = getArguments();
+            int position = bundle.getInt("position");
+            String imageFileName = IMAGE_NAME[position];
+            int imgResId = getResources().getIdentifier(imageFileName, "drawable", "com.example.akshaykumar.learnandroid");
+            imageView.setImageResource(imgResId);
+            return swipeView;
+        }
+
+        static SwipeFragment newInstance(int position) {
+            SwipeFragment swipeFragment = new SwipeFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            swipeFragment.setArguments(bundle);
+            return swipeFragment;
+        }
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+//            Intent intent = new Intent(MainActivity.this, YoutubeVideo.class);
+//            startActivity(intent);
+
+            startActivity(YouTubeStandalonePlayer.createVideoIntent(TestActivity.this,
+                    "AIzaSyDPgNoQdmeCOx6vb-DNhFlY0wqiFwVRQpU", "mcFmBcC4cPc", 0, true, false));
+        }
+
+    }
+
+    private void selectItem(int position) {
+        mDrawerList.setItemChecked(position, true);
+        mDrawerList.setSelection(position);
+        setTitle(mNavigationDrawerItemTitles[position - 1]);
+        mDrawerLayout.closeDrawers();
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    void setupToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    void setupDrawerToggle() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        //This is necessary to change the icon of the Drawer Toggle upon state change.
+        mDrawerToggle.syncState();
+    }
+
+}
